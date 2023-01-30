5800cbd100370d6302fbd3c35b71d7ab7556a380
Kirill Isaev <dexem@mail.ru>
2023-01-30 19:11:43 +0300

    testwork

---
+++ src/Component/Header/Navigation/index.js
@@ -0,0 +1,23 @@
+const Navigation = () => {
+  return (
+    
+      <nav>
+        <ul className="linkNavigation">
+          <li>
+            <button className="buttonNavigation">Классификатор услуг</button>
+          </li>
+          <li >
+            <button className="buttonNavigation">Архив услуг</button>
+          </li>
+          <li>
+            <button className="buttonNavigation">Пользователи</button>
+          </li>
+          <li>
+            <button className="buttonNavigation">Администрирование</button>
+          </li>
+        </ul>
+      </nav>
+
+  );
+};
+export default Navigation;
